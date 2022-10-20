25741dc916189f0fecba3743cc49a0d7b37bc9c7
ics-furudate <furudate@ics-web.jp>
2022-10-20 16:05:52 +0900

    build

--- docs/assets/demo03.js
+++ docs/assets/demo03.js
@@ -1,4 +1,4 @@
-(function polyfill() {
+/* empty css       */(function polyfill() {
   const relList = document.createElement("link").relList;
   if (relList && relList.supports && relList.supports("modulepreload")) {
     return;
@@ -39,4 +39,32 @@
     fetch(link.href, fetchOpts);
   }
 })();
-const style = "";
+const content = document.querySelector(".js-dialog-content");
+const open = document.querySelector(".js-dialog-open");
+const close = document.querySelector(".js-dialog-close");
+open.addEventListener("click", () => {
+  if (typeof content.showModal === "function") {
+    content.showModal();
+  } else {
+    alert("The <dialog> API is not supported by this browser");
+  }
+});
+close.addEventListener("click", () => {
+  content.close();
+  document.body.classList.remove("is-hideScrollbar");
+});
+const menuContent = document.querySelector(".js-menuDialog-content");
+const menuOpen = document.querySelector(".js-menuDialog-open");
+const menuClose = document.querySelector(".js-menuDialog-close");
+menuOpen.addEventListener("click", () => {
+  if (typeof menuContent.showModal === "function") {
+    menuContent.showModal();
+    document.body.classList.add("is-hideScrollbar");
+  } else {
+    alert("The <dialog> API is not supported by this browser");
+  }
+});
+menuClose.addEventListener("click", () => {
+  menuContent.close();
+  document.body.classList.remove("is-hideScrollbar");
+});
