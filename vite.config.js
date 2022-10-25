2d25affbebaaa7d0e1ead93c4d41d25214a92dd9
ics-furudate <furudate@ics-web.jp>
2022-10-25 10:31:10 +0900

    ルート要素のデモ削除を取り消し ／ CSSの微調整

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
         demo01: resolve(__dirname, "src/demo01/index.html"),
         demo02: resolve(__dirname, "src/demo02/index.html"),
         demo03: resolve(__dirname, "src/demo03/index.html"),
+        demo04: resolve(__dirname, "src/demo04/index.html"),
         iframe: resolve(__dirname, "src/iframe/iframe.html"),
       },
       output: {
